901892740d4e8693ea4879c56fa4c654e868cc23
Electr0n <skorodumov.eugene@gmail.com>
2017-03-22 17:56:04 +0300

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  
+  # REVIEW
+  # protect_from_forgery with: :null_session
+  def index
+    # info = data
+    # myFunc(JSON.stringify("Hello" + info.name))
+    myJSON = '{"name":"John", "age":30, "city":"New York"}'
+    render json: myJSON
+  end
+
 end
