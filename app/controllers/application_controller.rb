1851142b0b2b353e1ae7f33e10e9a08062186b37
Electr0n <skorodumov.eugene@gmail.com>
2017-03-23 16:54:49 +0300

    4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,8 @@ class ApplicationController < ActionController::API
   # REVIEW
   # protect_from_forgery with: :null_session
   def index
-    # info = data
-    # myFunc(JSON.stringify("Hello" + info.name))
-    # binding.pry
     data = params['data'].split('/?callback=')
     myJSON = JSON.parse data.first
-    # if myJSON["age"].to_i < 18
-    #   msg = "Hello " + myJSON["name"] + "! You are old enough. Welcome."
-    # else
-    #   msg = "Hello " + myJSON["name"] + "! You are too young."
-    # end
     myJSON["msg"] = myJSON["age"].to_i < 18 ? ("Hello " + myJSON["name"] + "! You are too young.") : ("Hello " + myJSON["name"] + "! You are old enough. Welcome.")
     render json: myJSON, callback: data.last
   end
